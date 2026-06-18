6092a3b239aad9908b7501eeffa9870fb389b64d
Gabriela Kotseva <gkotseva98@gmail.com>
2026-06-18 16:30:19 +0300

    new router for login and /me added

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,10 +1,21 @@
 import express from "express";
-import { getUsers, createUser } from "../controllers/user.controller.js";
-import { validateRegister } from "../middlewares/user.middleware.js";
+import {
+  getUsers,
+  createUser,
+  loginUser,
+  getMe,
+} from "../controllers/user.controller.js";
+import {
+  validateRegister,
+  validateLogin,
+  authenticate,
+} from "../middlewares/user.middleware.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
+router.get("/me", authenticate, getMe);
 router.post("/register", validateRegister, createUser);
+router.post("/login", validateLogin, loginUser);
 
 export default router;
