ca222ba7d700459d13b760721936a1cdb5e6b4f8
Gabriela Kotseva <gkotseva98@gmail.com>
2026-08-03 14:49:44 +0300

    Validation middleware created
    Created a middleware for validation of data coming from the frontend input fields upon register and login

--- server/middlewares/user.middleware.js
+++ server/middlewares/user.middleware.js
@@ -1,14 +1,17 @@
-export const validateRegister = (req, res, next) => {
-  const { name, email, password } = req.body;
+import jwt from "jsonwebtoken";
 
-  if (!name || !email || !password) {
+export const validateRegister = (req, res, next) => {
+  const { firstName, lastName, email, password, confirmPassword } = req.body;
+
+  if (!firstName || !lastName || !email || !password) {
     return res.status(400).json({
-      message: "name, email and password are required",
+      message: "First name, last name, email and password are required",
     });
   }
 
   if (
-    typeof name !== "string" ||
+    typeof firstName !== "string" ||
+    typeof lastName !== "string" ||
     typeof email !== "string" ||
     typeof password !== "string"
   ) {
@@ -17,7 +20,7 @@ export const validateRegister = (req, res, next) => {
     });
   }
 
-  if (name.trim().length < 2) {
+  if (firstName.trim().length < 2 || lastName.trim().length < 2) {
     return res.status(400).json({
       message: "Name too short",
     });
@@ -29,6 +32,12 @@ export const validateRegister = (req, res, next) => {
     });
   }
 
+  if (password !== confirmPassword) {
+    return res.status(400).json({
+      message: "Passwords don't match.",
+    });
+  }
+
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailRegex.test(email)) {
     return res.status(400).json({
@@ -37,4 +46,53 @@ export const validateRegister = (req, res, next) => {
   }
 
   next();
+};
+
+export const validateLogin = (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      message: "email and password are required",
+    });
+  }
+
+  if (typeof email !== "string" || typeof password !== "string") {
+    return res.status(400).json({
+      message: "Invalid data types",
+    });
+  }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({
+      message: "Invalid email format",
+    });
+  }
+
+  next();
+};
+
+export const authenticate = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({
+      message: "No token provided",
+    });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid token",
+    });
+  }
 };
