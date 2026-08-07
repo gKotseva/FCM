11d3bebb1193a0e519217d1206fe472ce5d79805
Gabriela Kotseva <gkotseva98@gmail.com>
2026-08-07 13:40:44 +0300

    updated testing script and db config to use test database

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,17 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
-dotenv.config();
+const envFile = process.env.NODE_ENV === "test" ? ".env.test" : ".env";
+
+dotenv.config({
+  path: envFile,
+});
+
+console.log({
+  NODE_ENV: process.env.NODE_ENV,
+  DB_NAME: process.env.DB_NAME,
+  DB_USER: process.env.DB_USER,
+});
 
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
